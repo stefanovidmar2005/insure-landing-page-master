73d758e087183eba8c5f70dfac67e071658e0f6a
stefanovidmar2005 <stefanovidmar@gmail.com>
2023-01-29 00:38:32 +0200

    Final mobile responsive and mobile nav working

---
+++ Javascript/app.js
@@ -0,0 +1,29 @@
+"use strict";
+// variable declarations
+const hamburgerBtn = document.querySelector(".hamburger");
+const body = document.querySelector("body");
+const menu = document.querySelector(".mobile__navigation");
+const overlay = document.querySelector(".overlay");
+let host = window.location.host;
+// close the modal window
+const closeModal = function () {
+  menu.classList.toggle("hidden");
+  body.classList.toggle("no-scroll");
+  hamburgerBtn.src = "../images/icon-hamburger.svg";
+};
+hamburgerBtn.addEventListener("click", () => {
+  menu.classList.toggle("hidden");
+  body.classList.toggle("no-scroll");
+  // changes the image source
+  if (hamburgerBtn.src === `http://${host}/images/icon-hamburger.svg`) {
+    hamburgerBtn.src = "../images/icon-close.svg";
+  } else {
+    hamburgerBtn.src = "../images/icon-hamburger.svg";
+  }
+});
+// escape key or delete on press closes the mobile navigator
+body.addEventListener("keydown", (e) => {
+  if (e.key === "Escape" || e.key === "Delete") {
+    closeModal();
+  }
+});
